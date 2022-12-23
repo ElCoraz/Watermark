a750abd2794ea4e380c74d0351f06171cd0ff44c
ElCoraz <dmwhite19850908@gmail.com>
2022-12-23 15:31:52 +0600

    s

--- src/main/java/com/elcorazon/adminlte/repository/WatermarkRepository.java
+++ src/main/java/com/elcorazon/adminlte/repository/WatermarkRepository.java
@@ -1,6 +1,6 @@
 package com.elcorazon.adminlte.repository;
 
-import com.elcorazon.adminlte.model.settings.Watermark;
+import com.elcorazon.adminlte.model.database.Watermark;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 /**********************************************************************************************************************/
