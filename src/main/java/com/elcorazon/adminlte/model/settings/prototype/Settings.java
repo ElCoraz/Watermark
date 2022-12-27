453c06465a668b0c07b82d67295067623ee8fcb5
ElCoraz <dmwhite19850908@gmail.com>
2022-12-27 19:07:03 +0600

    fix

--- src/main/java/com/elcorazon/adminlte/model/settings/prototype/Settings.java
+++ src/main/java/com/elcorazon/adminlte/model/settings/prototype/Settings.java
@@ -1,6 +1,5 @@
 package com.elcorazon.adminlte.model.settings.prototype;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
